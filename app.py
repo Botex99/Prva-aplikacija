19a7639aed1863d588761765d880e338a149a1fd
Botex99 <99490511+Botex99@users.noreply.github.com>
2024-01-30 16:56:04 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+
+
+db =SQLAlchemy(app)
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key =True)
+    username = db.Column(db.String)
+    text = db.Column(db.String)
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/opis")
+def omeni():
+    return render_template("opis.html")
+
+
+@app.route("/blog", methods=["GET"])
+def blog():
+
+    message = Message.query.all()
+
+    return render_template("blog.html", message=message)
+
+ 
+
+@app.route("/add-message", methods=["POST"])
+def add_message():
+
+    username = request.form.get("username")
+    text = request.form.get("text")
+
+    message = Message(username = username, text = text)
+
+    db.session.add(message)
+    db.session.commit()
+
+    return redirect("/blog")
+
+@app.route("/delete/<msg_id>", methods=["GET"])
+def delete_message(msg_id):
+
+    message = Message.query.get(int(msg_id))
+    
+    db.session.delete(message)
+    db.session.commit()
+
+    return redirect("/blog")
+
+    
+if __name__ == "__main__":
+    app.run()
+
